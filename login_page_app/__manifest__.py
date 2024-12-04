4d3bb21f68b1be24db45198618889530832ca4da
Cremisoft <info@cremisoft.com>
2024-12-04 13:37:10 +0000

    [ADD]login_page_app.

---
+++ login_page_app/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Part of BeyonData. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'login page redirect',
+    'version': '17.0.0.1',
+    'category': 'Survey',
+    'author': 'Shan',
+    'depends': ['base', 'auth_signup', 'website'],
+    'data': [
+        'views/res_user.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
